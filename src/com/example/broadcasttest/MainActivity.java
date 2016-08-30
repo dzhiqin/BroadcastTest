f249fe0fbcf767e6826aae6c2d1a8151768dc447
dzhiqin <dzhiqin@126.com>
2016-08-30 15:32:06 +0800

    测试广播demo

--- src/com/example/broadcasttest/MainActivity.java
+++ src/com/example/broadcasttest/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.broadcasttest;
 
 import java.util.Calendar;
 
+import com.example.broadcasttest.receiver.MyBroadcastReceiver;
+
 import android.app.Activity;
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -10,6 +12,8 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -19,17 +23,42 @@ import android.widget.Toast;
 public class MainActivity extends Activity {
 
 	private IntentFilter intentFilter;
+	private IntentFilter localIntentFilter;
+	private IntentFilter globalIntentFilter;
 	private NetworkChangeReceiver networkChangeReceiver;
+	private LocalReceiver localReceiver;
+	/**
+	 * 全局广播接收器
+	 */
+	private MyBroadcastReceiver myBroadcastReceiver;
+	/**
+	 * 本地广播管理器
+	 */
+	private LocalBroadcastManager localBroadcastManager;
 	private Button btnSendBroadcast;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		btnSendBroadcast=(Button)findViewById(R.id.btn_send);
+		//
+		/*myBroadcastReceiver=new MyBroadcastReceiver();
+		globalIntentFilter=new IntentFilter();
+		globalIntentFilter.addAction("broadcast_mybroadcast");
+		registerReceiver(myBroadcastReceiver,globalIntentFilter);
+		//动态注册广播监听
 		intentFilter=new IntentFilter();
 		intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
 		networkChangeReceiver=new NetworkChangeReceiver();
-		registerReceiver(networkChangeReceiver,intentFilter);
+		registerReceiver(networkChangeReceiver,intentFilter);*/
+		//获取本地广播管理器实例
+		localBroadcastManager=LocalBroadcastManager.getInstance(this);
+		//动态注册本地广播监听
+		localIntentFilter=new IntentFilter();
+		localIntentFilter.addAction("local_broadcast");
+		localReceiver=new LocalReceiver();
+		localBroadcastManager.registerReceiver(localReceiver, localIntentFilter);
+		//按键发送广播
 		btnSendBroadcast.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
@@ -38,7 +67,13 @@ public class MainActivity extends Activity {
 				//Create an intent with a given action
 				Intent intent=new Intent("broadcast_mybroadcast");
 				intent.putExtra("theDate", getDate());
-				sendBroadcast(intent);
+				//sendBroadcast(intent);
+				//发送有序广播
+				sendOrderedBroadcast(intent,null);
+				//发送本地广播
+				//Intent i=new Intent("local_broadcast");
+				//localBroadcastManager.sendBroadcast(i);
+				
 			}
 		});
 		
@@ -62,7 +97,14 @@ public class MainActivity extends Activity {
 	protected void onDestroy(){
 		super.onDestroy();
 		unregisterReceiver(networkChangeReceiver);
+		localBroadcastManager.unregisterReceiver(localReceiver);
+		unregisterReceiver(myBroadcastReceiver);
 	}
+	/**
+	 * 用于监听网络变化情况的广播接收器
+	 * @author Administrator
+	 *
+	 */
 	class NetworkChangeReceiver extends BroadcastReceiver{
 
 		@Override
@@ -80,7 +122,18 @@ public class MainActivity extends Activity {
 		}
 		
 	}
-	                                            
+	 /**
+	  * 本地广播接收器                                           
+	  */
+	class LocalReceiver extends BroadcastReceiver{
+
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Log.v("test","get localBroadcast");
+			
+		}
+		
+	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
--- src/com/example/broadcasttest/receiver/MyBroadcastReceiver.java
+++ src/com/example/broadcasttest/receiver/MyBroadcastReceiver.java
@@ -3,6 +3,7 @@ package com.example.broadcasttest.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MyBroadcastReceiver extends BroadcastReceiver{
@@ -11,7 +12,8 @@ public class MyBroadcastReceiver extends BroadcastReceiver{
 	public void onReceive(Context context, Intent intent) {
 		String theDate=intent.getStringExtra("theDate");
 		Toast.makeText(context, "get theDate="+theDate, Toast.LENGTH_SHORT).show();
-		
+		Log.v("test", "Receiver1 get broadcast");
+		//Sets the flag indicating that this receiver should abort the current broadcast
+		//abortBroadcast();
 	}
-
 }
